28f32c7e811c95dd2c961f1d583421ff54e3ab48
LAPTOP-7BJSCJKS\ashok <ashok.csecr@gmail.com>
2023-08-22 17:36:07 +0530

    mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,5 @@
-cp mongo.repo /etc/yum.repo.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
 # update listen address from 127.0.0.1 to 0.0.0.0
-systemctl enable mongodb
-systemctl restart mongodb
+systemctl enable mongod
+systemctl restart mongod
